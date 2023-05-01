20a3d3071a798cf83cd0bb17bfc5a3b29df5a066
Алина Жирохова <alinazirohova@MacBook-Air-Alina.local>
2023-05-01 13:44:25 +0300

    second commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,9 +2,7 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const bookRouter = require('./books');
 
-
 router.use('/users', userRouter);
 router.use('/books', bookRouter);
-
 
 module.exports = router;
